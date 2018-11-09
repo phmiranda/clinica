d7301315bda319fc35af9a45af130bc03c710b1e
pedro.miranda <pehhagah.1607@gmail.com>
2018-11-09 00:51:49 -0200

    inclusão das bibliotecas bootstrap e jquery.

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pedro
+ * Date: 09/11/2018
+ * Time: 00:50
+ */
+
+$msg = "CEP - Escolha Técnica de Ceilândia [Sistema de Administração de Medicamentos]";
+echo $msg;
